166e177d6eebc21db9fd6f753f31b24ae89367c5
Liliya Merzlyakova <norwegnwood@mail.ru>
2026-05-14 00:19:17 +0300

    Главный класс запуска приложения

---
+++ src/main/java/org/example/movieanalytics/MovieAnalyticsApplication.java
@@ -0,0 +1,11 @@
+package org.example.movieanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAnalyticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovieAnalyticsApplication.class, args);
+    }
+}
